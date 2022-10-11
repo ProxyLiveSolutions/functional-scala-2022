6ef9a45cafcfdccdf3e67466da33fd006bfcea7e
Aleksei Shamenev <ashamenev@onairent.live>
2022-10-11 15:39:31 +0300

    Init commit

---
+++ project/Dependencies.scala
@@ -0,0 +1,21 @@
+import sbt._
+import sbt.librarymanagement.ModuleID
+
+object Dependencies {
+  private type Deps = Seq[ModuleID]
+
+  private object Cats {
+    private val effectVer = "3.3.6"
+    private val coreVer   = "2.7.0"
+    private val org       = "org.typelevel"
+
+    val core   = org %% "cats-core"   % coreVer
+    val effect = org %% "cats-effect" % effectVer
+  }
+
+  private object FS2 {
+    val core = "co.fs2" %% "fs2-core" % "3.2.7"
+  }
+
+  val all: Deps = Seq(Cats.core, Cats.effect, FS2.core)
+}
---
+++ src/main/scala/com/onairentertainment/functional_scala_2022/Main.scala
@@ -0,0 +1,5 @@
+package com.onairentertainment.functional_scala_2022
+
+object Main:
+  def main(args: Array[String]): Unit =
+    println("Hellou!")
