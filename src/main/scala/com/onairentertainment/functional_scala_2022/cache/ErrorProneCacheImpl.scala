196278b2e3365a28fbe90c41cd7c2399caa78aa5
Aleksei Shamenev <ashamenev@onairent.live>
2022-11-21 12:40:09 +0200

    remove unrelated code

--- src/main/scala/com/onairentertainment/functional_scala_2022/cache/ErrorProneCacheImpl.scala
+++ src/main/scala/com/onairentertainment/functional_scala_2022/cache/ErrorProneCacheImpl.scala
@@ -28,31 +28,3 @@ private final class ErrorProneCacheImpl[F[_], K, V](
   override def get(key: K): F[Option[V]]                 = wrapWithError(underlying.get(key))
   override def insert(key: K, value: V): F[InsertResult] = wrapWithError(underlying.insert(key, value))
   override def update(key: K, value: V): F[UpdateResult] = wrapWithError(underlying.update(key, value))
-
-object Main extends App:
-  final case class Accum(sieve: Sieve, foundNumbers: List[Int])
-
-  type Sieve = SortedMap[Int, Boolean]
-  private val N = 100
-  @tailrec
-  private def dropFromSieve(elem: Int, offset: Int, sieve: Sieve): Sieve =
-    if (elem <= N)
-      dropFromSieve(elem + offset, offset, sieve.removed(elem))
-    else
-      sieve
-
-  val fullSieve = SortedMap.empty[Int, Boolean] ++ (2 to N).map(index => (index, true)).toMap
-  val numbers   = fullSieve.keys.toList
-  val zero      = Accum(fullSieve, List.empty)
-  val result = numbers.foldLeft(zero) { (acc, elem) =>
-    val foundNumbers =
-      if (acc.sieve.contains(elem))
-        acc.foundNumbers.prepended(elem)
-      else
-        acc.foundNumbers
-
-    val nextSieve = dropFromSieve(elem, elem, acc.sieve)
-    Accum(nextSieve, foundNumbers)
-  }
-
-  println(result.foundNumbers.reverse)
